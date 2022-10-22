4365813d2d98cece0dd9723cc1510fb8c48c72cb
andrei.kandratsenka <anddrews@tut.by>
2022-10-22 11:03:29 +0300

    added pagination

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,19 @@
 //here will by JavaScript code ...
+function simpleTemplating(data) {
+    var html = '<ul>';
+    $.each(data, function(index, item){
+        html += '<li><h2>'+ item +'</h2></li>';
+    });
+    html += '</ul>';
+    return html;
+}
+const dataContainer = document.querySelector('#data-container');
+$('#demo').pagination({
+    dataSource: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 13,14, 195],
+    pageSize: 10,
+    callback: function(data, pagination) {
+        // template method of yourself
+        var html = simpleTemplating(data);
+        $(dataContainer).html(html);
+    }
+})
