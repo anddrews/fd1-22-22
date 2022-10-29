00ac4cb628590401c6646863c267c56da8b6951e
andrei.kandratsenka <anddrews@tut.by>
2022-10-29 13:17:37 +0300

    initial commit

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,18 @@
-//here will by JavaScript code ...
+$(document).ready(
+    function(){
+        $('#slider').slick({
+            // autoplay: true,
+            autoplaySpeed: 1000,
+            appendArrows: $('.slider-arrows'),
+            // dots: true,
+            infinite: false
+        });
+        
+        $('#slider-secondary').slick({
+            // autoplay: true,
+            autoplaySpeed: 1000,
+            // dots: true,
+            infinite: true
+        });
+    } 
+  );
