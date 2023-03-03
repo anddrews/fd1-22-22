dca5d62500567d5e2ed8baa9c018c10df23390c9
Andrei_Kandratsenka <andrei_kandratsenka@epam.com>
2023-03-03 22:57:39 +0200

    firebase auth

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,32 @@
-//here will by JavaScript code ...
-function simpleTemplating(data) {
-    var html = '<ul>';
-    $.each(data, function(index, item){
-        html += '<li><h2>'+ item +'</h2></li>';
-    });
-    html += '</ul>';
-    return html;
+import {initializeApp} from 'https://www.gstatic.com/firebasejs/9.17.2/firebase-app.js';
+import {getAuth, createUserWithEmailAndPassword, sendEmailVerification, signInWithEmailAndPassword, signOut} from 'https://www.gstatic.com/firebasejs/9.17.2/firebase-auth.js';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC2X72pO_Nh0u93El8rt9SWf5RbFGGKhxA",
+  authDomain: "it-academy-auth.firebaseapp.com",
+  projectId: "it-academy-auth",
+  storageBucket: "it-academy-auth.appspot.com",
+  messagingSenderId: "543578464885",
+  appId: "1:543578464885:web:e9005bd63c99a8614c4e3c"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app)
+
+const signup = async (email, password) => {
+    await createUserWithEmailAndPassword(auth, email, password)
+    await sendEmailVerification(auth.currentUser)
 }
-const dataContainer = document.querySelector('#data-container');
-$('#demo').pagination({
-    dataSource: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 13,14, 195],
-    pageSize: 10,
-    callback: function(data, pagination) {
-        // template method of yourself
-        var html = simpleTemplating(data);
-        $(dataContainer).html(html);
-    }
-})
+
+const signin = (email, password) => signInWithEmailAndPassword(auth, email, password);
+const signout = () => signOut(auth)
+
+const application = async() => {
+    await signin('gispapirke@gufum.com', 'test123');
+
+    console.log(auth.currentUser);
+}
+
+application();
+
+// signup('gispapirke@gufum.com', 'test123');
